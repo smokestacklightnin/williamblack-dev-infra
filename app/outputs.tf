0b8d19168902dd12215839df21a9e8fcf94160c8
smokestacklightnin <125844868+smokestacklightnin@users.noreply.github.com>
2026-05-02 23:04:32 -0700

    Mark select outputs as sensitive

--- app/outputs.tf
+++ app/outputs.tf
@@ -1,13 +1,16 @@
 output "website_bucket" {
-  value = google_storage_bucket.website.name
+  value     = google_storage_bucket.website.name
+  sensitive = true
 }
 
 output "gha_deploy_sa" {
-  value = google_service_account.gha_deploy.email
+  value     = google_service_account.gha_deploy.email
+  sensitive = true
 }
 
 output "wif_provider" {
-  value = google_iam_workload_identity_pool_provider.github.name
+  value     = google_iam_workload_identity_pool_provider.github.name
+  sensitive = true
 }
 
 output "cloudflare_nameservers" {
