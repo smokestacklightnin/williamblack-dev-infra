d7e551716b4b67069a0f8c1c463a1d5593ba094e
William Black <125844868+smokestacklightnin@users.noreply.github.com>
2026-05-02 22:44:01 -0700

    Add roles/storage.legacyBucketReader role to gha-deploy service account (#8)
    This is required by `gcloud storage` command

--- app/iam.tf
+++ app/iam.tf
@@ -9,3 +9,9 @@ resource "google_storage_bucket_iam_member" "gha_deploy_object_admin" {
   role   = "roles/storage.objectUser"
   member = "serviceAccount:${google_service_account.gha_deploy.email}"
 }
+
+resource "google_storage_bucket_iam_member" "gha_deploy_bucket_reader" {
+  bucket = google_storage_bucket.website.name
+  role   = "roles/storage.legacyBucketReader"
+  member = "serviceAccount:${google_service_account.gha_deploy.email}"
+}
